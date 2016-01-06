d37956b2a984cc0be59e896bbd93d7d6ade9aaa8
kannanParamasivam <kannan.paramasivam@live.com>
2016-01-06 07:39:39 -0600

    Solution Renamed and Day2 added

---
+++ Code Project 7 days/Lab1/Day 2/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Day_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day_2.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Sending data from controller to View using ViewData
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetView()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Kannan";
+            emp.LastName = "Paramasivam";
+            emp.Salary = 5000;
+            ViewData["Employee"] = emp;
+
+            return View("myview");
+        }
+    }
+}
---
+++ Code Project 7 days/Lab1/Day 2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Day_2.Startup))]
+namespace Day_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
